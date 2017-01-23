adc991bb005256e36c784943802447da0246af8d
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-01-22 22:12:51 -0500

    So it works when there's no variable hmmm

--- witCommands/witCommands.py
+++ witCommands/witCommands.py
@@ -61,7 +61,7 @@ def create_client(actions):
 
 def get_response(message, vactions=actions):
     print("GE RESPONSE")
-    session_id = 'my-user-session-50'
+    session_id = 'my-user-session-51'
     context0 = {}
     wit_client = create_client(actions)
     resp = wit_client.message(message)
